1967f9a1bbc0fe56335253b1037228b8e6c21fa2
Fabien Jaegle <fjaegle@divalto.com>
2022-08-24 09:55:39 +0200

    Add reviews to recipe detail

--- client/src/components/RecipeDetail.js
+++ client/src/components/RecipeDetail.js
@@ -1,20 +1,19 @@
-import React from "react";
+import React, {useState} from "react";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
   import { faMortarPestle, faFireBurner, faBed } from '@fortawesome/free-solid-svg-icons'
 
-class RecipeDetail extends React.Component {
-    constructor(props) {
-        super(props);
-   
-        this.state = {
-            item: this.props.recipe
-        };
-    }
+const RecipeDetail = (props) => {
+    const [activeTab, setActiveTab] = useState("directions");
+    const handleTabDirections = () => {
+        setActiveTab("directions");
+      };
+      const handleTabReviews = () => {
+        setActiveTab("reviews");
+      };
+        const item = props.recipe;
 
-    render() {
-        const { item } = this.state;
         return (
-            <div className="section pt-4">
+        <div className="section pt-4">
             <div className="container pb-4">   
                 <div className="courses-details-wrapper">
                     <div className="row flex-row-reverse gx-xxl-5">
@@ -38,128 +37,53 @@ class RecipeDetail extends React.Component {
                                     </div>
                                 </div>
 
+                                <p>{item.description}</p>
+
                                 <div className="courses-details-tab">
                                     <div className="details-tab-menu">
                                         <ul className="nav justify-content-center">
-                                            <li><button className="active" data-bs-toggle="tab" data-bs-target="#description">Description</button></li>
-                                            <li><button data-bs-toggle="tab" data-bs-target="#reviews">Reviews</button></li>
+                                            <li><button className={activeTab === "directions" ? "active" : ""} onClick={handleTabDirections}>Etapes</button></li>
+                                            <li><button className={activeTab === "reviews" ? "active" : ""} onClick={handleTabReviews}>Avis</button></li>
                                         </ul>
                                     </div>
 
                                     <div className="details-tab-content">
                                         <div className="tab-content">
-                                            <div className="tab-pane fade show active" id="description">
+                                        
+                                            <div className={activeTab === "directions" ? "tab-pane fade show active" : "tab-pane fade"} id="directions">
                                                 <div className="tab-description">
                                                     <div className="description-wrapper">
-                                                        <h3 className="tab-title">Description:</h3>
-                                                        <p>{item.description}</p>
+                                                        <p>{item.directions}</p>
                                                     </div>
                                                 </div>
                                             </div>
-                                            <div className="tab-pane fade" id="reviews">
+                                            <div className={activeTab === "reviews" ? "tab-pane fade show active" : "tab-pane fade"} id="reviews">
                                                 <div className="tab-reviews">
-                                                    <h3 className="tab-title">Student Reviews:</h3>
-{/*
-                                                    <div className="reviews-wrapper reviews-active">
-                                                        <div className="swiper-container swiper-container-initialized swiper-container-horizontal swiper-container-pointer-events">
-                                                            <div className="swiper-wrapper" id="swiper-wrapper-222bef7cfec6b22c" aria-live="off" style="transform: translate3d(-2394px, 0px, 0px); transition-duration: 0ms;"><div className="single-review swiper-slide swiper-slide-duplicate swiper-slide-duplicate-active" data-swiper-slide-index="2" style="width: 768px; margin-right: 30px;" role="group" aria-label="1 / 5">
-                                                                    <div className="review-author">
-                                                                        <div className="author-thumb">
-                                                                            <img src="assets/images/author/author-03.jpg" alt="Author" />
-                                                                            <i className="icofont-quote-left"></i>
-                                                                        </div>
-                                                                        <div className="author-content">
-                                                                            <h4 className="name">Gertude Culbertson</h4>
-                                                                            <span className="designation">Product Designer, USA</span>
-                                                                            <span className="rating-star">
-                                                                            <span className="rating-bar" style={{width: 80 + '%'}}></span>
-                                                                            </span>
-                                                                        </div>
+                                                    {
+                                                        item.reviews.map((review) => (
+                                                            <div key={review.id} className="single-review mb-4" role="group">
+                                                                <div className="review-author">
+                                                                    <div className="author-content">
+                                                                        <h4 className="name">{review.name}</h4>
+                                                                        <span className="rating-star">
+                                                                            <span className="rating-bar" style={{width: (review.rate * 100 / 5) + '%'}}></span>
+                                                                        </span>
                                                                     </div>
-                                                                    <p>Lorem Ipsum has been the industry's standard dummy text since the 1500 when unknown printer took a galley of type and scrambled to make type specimen book has survived not five centuries but also the leap into electronic type and book.</p>
                                                                 </div>
-
-                                                                <div className="single-review swiper-slide swiper-slide-duplicate-next" data-swiper-slide-index="0" style="width: 768px; margin-right: 30px;" role="group" aria-label="2 / 5">
-                                                                    <div className="review-author">
-                                                                        <div className="author-thumb">
-                                                                            <img src="assets/images/author/author-06.jpg" alt="Author" />
-                                                                            <i className="icofont-quote-left"></i>
-                                                                        </div>
-                                                                        <div className="author-content">
-                                                                            <h4 className="name">Sara Alexander</h4>
-                                                                            <span className="designation">Product Designer, USA</span>
-                                                                            <span className="rating-star">
-                                                                            <span className="rating-bar" style={{width: 80 + '%'}}></span>
-                                                                            </span>
-                                                                        </div>
-                                                                    </div>
-                                                                    <p>Lorem Ipsum has been the industry's standard dummy text since the 1500 when unknown printer took a galley of type and scrambled to make type specimen book has survived not five centuries but also the leap into electronic type and book.</p>
-                                                                </div>
-
-                                                                <div className="single-review swiper-slide swiper-slide-prev" data-swiper-slide-index="1" style="width: 768px; margin-right: 30px;" role="group" aria-label="3 / 5">
-                                                                    <div className="review-author">
-                                                                        <div className="author-thumb">
-                                                                            <img src="assets/images/author/author-07.jpg" alt="Author" />
-                                                                            <i className="icofont-quote-left"></i>
-                                                                        </div>
-                                                                        <div className="author-content">
-                                                                            <h4 className="name">Karol Bachman</h4>
-                                                                            <span className="designation">Product Designer, USA</span>
-                                                                            <span className="rating-star">
-                                                                            <span className="rating-bar" style={{width: 60 + '%'}}></span>
-                                                                            </span>
-                                                                        </div>
-                                                                    </div>
-                                                                    <p>Lorem Ipsum has been the industry's standard dummy text since the 1500 when unknown printer took a galley of type and scrambled to make type specimen book has survived not five centuries but also the leap into electronic type and book.</p>
-                                                                </div>
-
-                                                                <div className="single-review swiper-slide swiper-slide-active" data-swiper-slide-index="2" style="width: 768px; margin-right: 30px;" role="group" aria-label="4 / 5">
-                                                                    <div className="review-author">
-                                                                        <div className="author-thumb">
-                                                                            <img src="assets/images/author/author-03.jpg" alt="Author" />
-                                                                            <i className="icofont-quote-left"></i>
-                                                                        </div>
-                                                                        <div className="author-content">
-                                                                            <h4 className="name">Gertude Culbertson</h4>
-                                                                            <span className="designation">Product Designer, USA</span>
-                                                                            <span className="rating-star">
-                                                                            <span className="rating-bar" style={{width: 80 + '%'}}></span>
-                                                                            </span>
-                                                                        </div>
-                                                                    </div>
-                                                                    <p>Lorem Ipsum has been the industry's standard dummy text since the 1500 when unknown printer took a galley of type and scrambled to make type specimen book has survived not five centuries but also the leap into electronic type and book.</p>
-                                                                </div>
-
-                                                                <div className="single-review swiper-slide swiper-slide-duplicate swiper-slide-next" data-swiper-slide-index="0" style="width: 768px; margin-right: 30px;" role="group" aria-label="5 / 5">
-                                                                    <div className="review-author">
-                                                                        <div className="author-thumb">
-                                                                            <img src="assets/images/author/author-06.jpg" alt="Author" />
-                                                                            <i className="icofont-quote-left"></i>
-                                                                        </div>
-                                                                        <div className="author-content">
-                                                                            <h4 className="name">Sara Alexander</h4>
-                                                                            <span className="designation">Product Designer, USA</span>
-                                                                            <span className="rating-star">
-                                                                            <span className="rating-bar" style={{width: 80 + '%'}}></span>
-                                                                            </span>
-                                                                        </div>
-                                                                    </div>
-                                                                    <p>Lorem Ipsum has been the industry's standard dummy text since the 1500 when unknown printer took a galley of type and scrambled to make type specimen book has survived not five centuries but also the leap into electronic type and book.</p>
-                                                                </div>
+                                                                <p>{review.comment}</p>
                                                             </div>
-                                                            <div className="swiper-pagination swiper-pagination-clickable swiper-pagination-bullets"><span className="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 1"></span><span className="swiper-pagination-bullet" tabindex="0" role="button" aria-label="Go to slide 2"></span><span className="swiper-pagination-bullet swiper-pagination-bullet-active" tabindex="0" role="button" aria-label="Go to slide 3"></span></div>
-                                                        <span className="swiper-notification" aria-live="assertive" aria-atomic="true"></span></div>
-                                                    </div>
-*/}
+                                                        ))
+                                                    }
+
                                                     <div className="reviews-btn">
-                                                        <button type="button" className="btn btn-dark btn-hover-primary" data-bs-toggle="modal" data-bs-target="#reviewsModal">Write A Review</button>
+                                                        <button type="button" className="btn btn-dark btn-hover-primary" data-bs-toggle="modal" data-bs-target="#reviewsModal">Ecrire un avis</button>
                                                     </div>
 
-                                                    <div className="modal fade" id="reviewsModal">
+                                                    {/*<div className="modal fade" id="reviewsModal">
                                                         <div className="modal-dialog modal-dialog-centered">
                                                             <div className="modal-content">
                                                                 <div className="modal-header">
-                                                                    <h5 className="modal-title">Add a Review</h5>
+                                                                    <h5 className="modal-title">Ajouter un avis</h5>
                                                                     <button type="button" className="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
                                                                 </div>
 
@@ -195,7 +119,7 @@ class RecipeDetail extends React.Component {
                                                                             </div>
                                                                             <div className="col-md-12">
                                                                                 <div className="single-form">
-                                                                                    <button className="btn btn-dark btn-hover-primary">Submit Review</button>
+                                                                                    <button className="btn btn-dark btn-hover-primary">Envoyer</button>
                                                                                 </div>
                                                                             </div>
                                                                         </div>
@@ -203,15 +127,12 @@ class RecipeDetail extends React.Component {
                                                                 </div>
                                                             </div>
                                                         </div>
-                                                    </div>
+                                                    </div>*/}
                                                 </div>
-
                                             </div>
                                         </div>
                                     </div>
-
                                 </div>
-
                             </div>
                         </div>
                         <div className="col-lg-4">
@@ -234,8 +155,7 @@ class RecipeDetail extends React.Component {
                 </div>
             </div>
         </div>
-        )
-    }
+    )
 }
 
 export default RecipeDetail;
