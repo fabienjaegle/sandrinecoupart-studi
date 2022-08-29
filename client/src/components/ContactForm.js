7503dfcae57ef48b120deb8c9908bca502eefffd
Fabien Jaegle <fjaegle@divalto.com>
2022-08-29 07:44:12 +0200

    Change contact form

--- client/src/components/ContactForm.js
+++ client/src/components/ContactForm.js
@@ -2,48 +2,45 @@ const ContactForm = () => {
     return (
 <div className="section section-padding">
             <div className="container">
-
                 <div className="contact-wrapper">
                     <div className="row justify-content-center">
                         <div className="col-lg-6">
                             <div className="contact-content">
-
                                 <div className="contact-title">
-                                    <h4 className="sub-title">Don’t worry!</h4>
-                                    <h2 className="main-title">If you have any query? Contact with us.</h2>
+                                    <h4 className="sub-title">Pas d'inquétude !</h4>
+                                    <h2 className="main-title">Vous avez une question ? Contactez-moi.</h2>
                                 </div>
-                                    
                                 <div className="form-shuvo">
                                     <form className="contact-form wow fadeInLeft" id="reused_form" action="POST">
                                         <div className="row">
                                             <div className="col-lg-6">
                                                 <div className="form-group mt-2">
-                                                    <input name="name" id="name" type="text" className="form-control" placeholder="Your Name*" required="" />
+                                                    <input name="name" id="name" type="text" className="form-control" placeholder="Votre nom*" required="" />
                                                 </div>
                                             </div>
                                             <div className="col-lg-6">
                                                 <div className="form-group mt-2">
-                                                    <input name="email" id="email" type="email" className="form-control" placeholder="Your Email*" required="" />
+                                                    <input name="email" id="email" type="email" className="form-control" placeholder="Votre adresse mail*" required="" />
                                                 </div>
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div className="col-lg-12">
                                                 <div className="form-group mt-2">
-                                                    <input type="text" className="form-control" name="subject" id="subject" placeholder="Your Subject.." required="" />
+                                                    <input type="text" className="form-control" name="subject" id="subject" placeholder="Votre sujet" required="" />
                                                 </div>
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div className="col-lg-12">
                                                 <div className="form-group mt-2">
-                                                    <textarea name="comments" id="comments" rows="4" className="form-control" placeholder="Your message..."></textarea>
+                                                    <textarea name="comments" id="comments" rows="4" className="form-control" placeholder="Votre message..."></textarea>
                                                 </div>
                                             </div>
                                         </div>
                                         <div className="row">
                                             <div className="col-lg-12 text-left mt-4">
-                                                <button type="submit" className="btn btn-secondary btn-hover-primary">Send Message  </button>
+                                                <button type="submit" className="btn btn-secondary btn-hover-primary">Envoyer</button>
                                             </div>
                                         </div>
                                     </form>
@@ -59,25 +56,17 @@ const ContactForm = () => {
                         <div className="col-lg-6">
                             <div className="contact-information">
                                 <div className="information-wrapper">
-
-                                    <img className="shape" src="assets/images/shape/shape-11.png" alt="" />
-
                                     <div className="single-info">
-                                        <h3 className="info-title">Phone:</h3>
-
-                                        <p>(765) 388-2142</p>
-                                        <p>(304) 855-3859</p>
+                                        <h3 className="info-title">Téléphone :</h3>
+                                        <p>06 12 23 45 56</p>
                                     </div>
                                     <div className="single-info">
-                                        <h3 className="info-title">Email:</h3>
-
-                                        <p>example@gmail.com</p>
-                                        <p>yourmail@gmail.com</p>
+                                        <h3 className="info-title">Adresse mail :</h3>
+                                        <p>contact@sandrine-coupart.com</p>
                                     </div>
                                     <div className="single-info">
-                                        <h3 className="info-title">Address:</h3>
-
-                                        <p>1229 Roma Ct San Jacinto, California(CA), 92583</p>
+                                        <h3 className="info-title">Adresse :</h3>
+                                        <p>4 quai de l'Orne, 14118 Caen</p>
                                     </div>
                                 </div>
                             </div>
