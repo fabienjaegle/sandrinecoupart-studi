241f5e4234b9757a948ef88aa11c627782bffcd4
Fabien Jaegle <fjaegle@divalto.com>
2022-09-07 13:14:14 +0200

    fix url

--- index.js
+++ index.js
@@ -5,6 +5,7 @@ import cors from "cors";
 import db from "./config/Database.js";
 import router from "./routes/index.js";
 import bodyparser from 'body-parser';
+const path = require("path");
 dotenv.config();
 const app = express();
 
