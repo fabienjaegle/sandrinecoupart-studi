d0bb1ab41d6dd8d23d35f15466e7d5d8a618358d
Fabien Jaegle <fjaegle@divalto.com>
2022-09-09 11:13:16 +0200

    Fix policy page

--- client/src/components/PolicyPage.js
+++ client/src/components/PolicyPage.js
@@ -7,20 +7,20 @@ const PolicyPage = () => {
                 </div>
                 <div className="col-12">
                     <p className="text-center">DONNÉES PERSONNELLES</p>
-                    <h3>Définitions</h3>
+                    <h3 className="mt-4 mb-4">Définitions</h3>
                     <p><strong>L'Éditeur</strong> : La personne, physique ou morale, qui édite les services de communication au publicen ligne.</p>
                     <p><strong>Le Site</strong> : L'ensemble des sites, pages Internet et services en ligne proposés par l'Éditeur.</p>
                     <p><strong>L'Utilisateur</strong> : La personne utilisant le Site et les services.</p>
-                    <h4>1- Nature des données collectées</h4>
+                    <h4 className="mt-4 mb-4">1- Nature des données collectées</h4>
                     <p>Dans le cadre de l'utilisation des Sites, l'Éditeur est susceptible de collecter les catégories de données suivantes concernant ses Utilisateurs :</p>
                     <ol>Données d'état-civil, d'identité, d'identification...</ol>
                     <ol>Données de connexion (adresses IP, journaux d'événements...)</ol>
-                    <h4>2- Communication des données personnelles à des tiers</h4>
-                    <p>Pas de communication à des tiers</p>
+                    <h4 className="mt-4 mb-4">2- Communication des données personnelles à des tiers</h4>
+                    <p><strong>Pas de communication à des tiers</strong></p>
                     <p>Vos données ne font l'objet d'aucune communication à des tiers. Vous êtes toutefois informés
                     qu'elles pourront être divulguées en application d'une loi, d'un règlement ou en vertu d'une
                     décision d'une autorité réglementaire ou judiciaire compétente.</p>
-                    <h4>3- Information préalable pour la communication des
+                    <h4 className="mt-4 mb-4">3- Information préalable pour la communication des
                     données personnelles à des tiers en cas de fusion /
                     absorption</h4>
                     <p>Information préalable et possibilité d’opt-out avant et après la fusion / acquisition
@@ -28,8 +28,8 @@ const PolicyPage = () => {
                     forme de cession d’actifs, nous nous engageons à garantir la confidentialité de vos données
                     personnelles et à vous informer avant que celles-ci ne soient transférées ou soumises à de
                     nouvelles règles de confidentialité.</p>
-                    <h4>4- Agrégation des données</h4>
-                    <p>Agrégation avec des données non personnelles</p>
+                    <h4 className="mt-4 mb-4">4- Agrégation des données</h4>
+                    <p><strong>Agrégation avec des données non personnelles</strong></p>
                     <p>Nous pouvons publier, divulguer et utiliser les informations agrégées (informations relatives à
                     tous nos Utilisateurs ou à des groupes ou catégories spécifiques d'Utilisateurs que nous
                     combinons de manière à ce qu'un Utilisateur individuel ne puisse plus être identifié ou
@@ -42,14 +42,14 @@ const PolicyPage = () => {
                     autre information dont vous avez autorisé la divulgation. Nous pouvons agréger les informations
                     relatives à tous nos autres Utilisateurs, groupes, comptes, aux données personnelles disponibles
                     sur l’Utilisateur.</p>
-                    <h4>5- Collecte des données d'identité</h4>
-                    <p>Consultation libre</p>
+                    <h4 className="mt-4 mb-4">5- Collecte des données d'identité</h4>
+                    <p><strong>Consultation libre</strong></p>
                     <p>La consultation du Site ne nécessite pas d'inscription ni d'identification préalable. Elle peut
                     s'effectuer sans que vous ne communiquiez de données nominatives vous concernant (nom,
                     prénom, adresse, etc). Nous ne procédons à aucun enregistrement de données nominatives
                     pour la simple consultation du Site.</p>
-                    <h4>6- Collecte des données d'identification</h4>
-                    <p>Utilisation de l'identifiant de l'utilisateur pour proposition de mise en relation et offres commerciales</p>
+                    <h4 className="mt-4 mb-4">6- Collecte des données d'identification</h4>
+                    <p><strong>Utilisation de l'identifiant de l'utilisateur pour proposition de mise en relation et offres commerciales</strong></p>
                     <p>Nous utilisons vos identifiants électroniques pour rechercher des relations présentes par
                     connexion, par adresse mail ou par services. Nous pouvons utiliser vos informations de contact
                     pour permettre à d'autres personnes de trouver votre compte, notamment via des services tiers
@@ -60,18 +60,18 @@ const PolicyPage = () => {
                     votre carnet d’adresses. Nous sommes susceptibles de travailler en partenariat avec des
                     sociétés qui proposent des offres incitatives. Pour prendre en charge ce type de promotion et
                     d'offre incitative, nous sommes susceptibles de partager votre identifiant électronique.</p>
-                    <h4>7- Collecte des données du terminal</h4>
-                    <p>Aucune collecte des données techniques</p>
+                    <h4 className="mt-4 mb-4">7- Collecte des données du terminal</h4>
+                    <p><strong>Aucune collecte des données techniques</strong></p>
                     <p>Nous ne collectons et ne conservons aucune donnée technique de votre appareil (adresse IP,
                     fournisseur d'accès à Internet...).</p>
-                    <h4>8- Cookies</h4>
-                    <p>Durée de conservation des cookies</p>
+                    <h4 className="mt-4 mb-4">8- Cookies</h4>
+                    <p><strong>Durée de conservation des cookies</strong></p>
                     <p>Conformément aux recommandations de la CNIL, la durée maximale de conservation des
                     cookies est de 13 mois au maximum après leur premier dépôt dans le terminal de l'Utilisateur,
                     tout comme la durée de la validité du consentement de l’Utilisateur à l’utilisation de ces cookies.
                     La durée de vie des cookies n’est pas prolongée à chaque visite. Le consentement de
                     l’Utilisateur devra donc être renouvelé à l'issue de ce délai.</p>
-                    <p>Finalité cookies</p>
+                    <p><strong>Finalité cookies</strong></p>
                     <p>Les cookies peuvent être utilisés pour des fins statistiques notamment pour optimiser les
                     services rendus à l'Utilisateur, à partir du traitement des informations concernant la fréquence
                     d'accès, la personnalisation des pages ainsi que les opérations réalisées et les informations
@@ -91,45 +91,45 @@ const PolicyPage = () => {
                     fonctionnement du service.</p>
                     <p>L'Éditeur peut être amenée à recueillir des informations de navigation via l'utilisation de
                     cookies.</p>
-                    <h4>9 - Conservation des données techniques</h4>
-                    <p>Durée de conservation des données techniques</p>
+                    <h4 className="mt-4 mb-4">9 - Conservation des données techniques</h4>
+                    <p><strong>Durée de conservation des données techniques</strong></p>
                     <p>Les données techniques sont conservées pour la durée strictement nécessaire à la réalisation
                     des finalités visées ci-avant.</p>
-                    <h4>10- Délai de conservation des données personnelles et d'anonymisation</h4>
-                    <p>Conservation des données pendant la durée de la relation contractuelle</p>
+                    <h4 className="mt-4 mb-4">10- Délai de conservation des données personnelles et d'anonymisation</h4>
+                    <p><strong>Conservation des données pendant la durée de la relation contractuelle</strong></p>
                     <p>Conformément à l'article 6-5° de la loi n°78-17 du 6 janvier 1978 relative à l'informatique, aux
                     fichiers et aux libertés, les données à caractère personnel faisant l'objet d'un traitement ne sont
                     pas conservées au-delà du temps nécessaire à l'exécution des obligations définies lors de la
                     conclusion du contrat ou de la durée prédéfinie de la relation contractuelle.</p>
-                    <p>Conservation des données anonymisées au delà de la relation contractuelle / après la
-                    suppression du compte</p>
+                    <p><strong>Conservation des données anonymisées au delà de la relation contractuelle / après la
+                    suppression du compte</strong></p>
                     <p>Nous conservons les données personnelles pour la durée strictement nécessaire à la réalisation
                     des finalités décrites dans les présentes Politiques de confidentialité. Au-delà de cette durée,
                     elles seront anonymisées et conservées à des fins exclusivement statistiques et ne donneront
                     lieu à aucune exploitation, de quelque nature que ce soit.</p>
-                    <p>Suppression des données après suppression du compte</p>
+                    <p><strong>Suppression des données après suppression du compte</strong></p>
                     <p>Des moyens de purge de données sont mis en place afin d'en prévoir la suppression effective
                     dès lors que la durée de conservation ou d'archivage nécessaire à l'accomplissement des
                     finalités déterminées ou imposées est atteinte. Conformément à la loi n°78-17 du 6 janvier 1978
                     relative à l'informatique, aux fichiers et aux libertés, vous disposez par ailleurs d'un droit de
                     suppression sur vos données que vous pouvez exercer à tout moment en prenant contact avec
                     l'Éditeur.</p>
-                    <p>Suppression des données après 3 ans d'inactivité</p>
+                    <p><strong>Suppression des données après 3 ans d'inactivité</strong></p>
                     <p>Pour des raisons de sécurité, si vous ne vous êtes pas authentifié sur le Site pendant une
                     période de trois ans, vous recevrez un e-mail vous invitant à vous connecter dans les plus brefs
                     délais, sans quoi vos données seront supprimées de nos bases de données.</p>
-                    <h4>11- Suppression du compte</h4>
-                    <p>Suppression du compte à la demande</p>
+                    <h4 className="mt-4 mb-4">11- Suppression du compte</h4>
+                    <p><strong>Suppression du compte à la demande</strong></p>
                     <p>L'Utilisateur a la possibilité de supprimer son Compte à tout moment, par simple demande à
                     l'Éditeur OU par le menu de suppression de Compte présent dans les paramètres du Compte le
                     cas échéant.</p>
-                    <p>Suppression du compte en cas de violation de la Politique de Confidentialité</p>
+                    <p><strong>Suppression du compte en cas de violation de la Politique de Confidentialité</strong></p>
                     <p>En cas de violation d'une ou de plusieurs dispositions de la Politique de Confidentialité ou de tout
                     autre document incorporé aux présentes par référence, l'Éditeur se réserve le droit de mettre fin
                     ou restreindre sans aucun avertissement préalable et à sa seule discrétion, votre usage et accès
                     aux services, à votre compte et à tous les Sites.</p>
-                    <h4>12- Indications en cas de faille de sécurité décelée par l'Éditeur</h4>
-                    <p>Information de l'Utilisateur en cas de faille de sécurité</p>
+                    <h4 className="mt-4 mb-4">12- Indications en cas de faille de sécurité décelée par l'Éditeur</h4>
+                    <p><strong>Information de l'Utilisateur en cas de faille de sécurité</strong></p>
                     <p>Nous nous engageons à mettre en oeuvre toutes les mesures techniques et organisationnelles
                     appropriées afin de garantir un niveau de sécurité adapté au regard des risques d'accès
                     accidentels, non autorisés ou illégaux, de divulgation, d'altération, de perte ou encore de
@@ -137,16 +137,16 @@ const PolicyPage = () => {
                     connaissance d'un accès illégal aux données personnelles vous concernant stockées sur nos
                     serveurs ou ceux de nos prestataires, ou d'un accès non autorisé ayant pour conséquence la
                     réalisation des risques identifiés ci-dessus, nous nous engageons à :</p>
-                    <ol>Vous notifier l'incident dans les plus brefs délais ;</ol>
-                    <ol>Examiner les causes de l'incident et vous en informer ;</ol>
+                    <ol>- Vous notifier l'incident dans les plus brefs délais ;</ol>
+                    <ol>- Examiner les causes de l'incident et vous en informer ;</ol>
                     <p>Prendre les mesures nécessaires dans la limite du raisonnable afin d'amoindrir les effets négatifs
                     et préjudices pouvant résulter dudit incident.</p>
-                    <p>Limitation de la responsabilité</p>
+                    <p><strong>Limitation de la responsabilité</strong></p>
                     <p>En aucun cas les engagements définis au point ci-dessus relatifs à la notification en cas de faille
                     de sécurité ne peuvent être assimilés à une quelconque reconnaissance de faute ou de
                     responsabilité quant à la survenance de l'incident en question.</p>
-                    <h4>13- Transfert des données personnelles à l'étranger</h4>
-                    <p>Transfert des données dans des pays avec un niveau de protection équivalent</p>
+                    <h4 className="mt-4 mb-4">13- Transfert des données personnelles à l'étranger</h4>
+                    <p><strong>Transfert des données dans des pays avec un niveau de protection équivalent</strong></p>
                     <p>L'Éditeur s'engage à respecter la réglementation applicable relative aux transferts des
                     données vers des pays étrangers et notamment selon les modalités suivantes :</p>
                     <p>L'Éditeur transfère les données personnelles de ses Utilisateurs vers des pays reconnus
@@ -154,23 +154,22 @@ const PolicyPage = () => {
                     <p>L'Éditeur transfère les données personnelles de ses Utilisateurs en dehors des pays
                     reconnus par la CNIL comme ayant un niveau de protection suffisant : L'Éditeur a obtenu
                     une autorisation de la CNIL pour procéder à ce transfert.</p>
-                    <p>Pour connaître la liste de ces pays : CNIL - La protection des données dans le monde
-                    https://www.cnil.fr/fr/la-protection-des-donnees-dans-le-mondehttps://www.cnil.fr/fr/laprotection-des-donnees-dans-le-monde</p>
-                    <h4>14- Modification de la politique de confidentialité</h4>
+                    <p>Pour connaître la liste de ces pays : CNIL - La protection des données dans le monde <a href="https://www.cnil.fr/fr/la-protection-des-donnees-dans-le-monde">https://www.cnil.fr/fr/laprotection-des-donnees-dans-le-monde</a></p>
+                    <h4 className="mt-4 mb-4">14- Modification de la politique de confidentialité</h4>
                     <p>En cas de modification de la présente Politique de Confidentialité, engagement de ne pas
                     baisser le niveau de confidentialité de manière substantielle sans l'information préalable
-                    des personnes concernées</p>
+                    des personnes concernées.</p>
                     <p>Nous nous engageons à vous informer en cas de modification substantielle de la présente
                     Politique de Confidentialité, et à ne pas baisser le niveau de confidentialité de vos données de
                     manière substantielle sans vous en informer et obtenir votre consentement.</p>
-                    <h4>15- Droit applicable et modalités de recours</h4>
-                    <p>Clause d'arbitrage</p>
+                    <h4 className="mt-4 mb-4">15- Droit applicable et modalités de recours</h4>
+                    <p><strong>Clause d'arbitrage</strong></p>
                     <p>Vous acceptez expressément que tout litige susceptible de naître du fait de la présente Politique
                     de Confidentialité, notamment de son interprétation ou de son exécution, relèvera d'une
                     procédure d'arbitrage soumise au règlement de la plateforme d'arbitrage choisie d'un commun
                     accord, auquel vous adhérerez sans réserve.</p>
-                    <h4>16- Portabilité des données</h4>
-                    <p>Portabilité des données</p>
+                    <h4 className="mt-4 mb-4">16- Portabilité des données</h4>
+                    <p><strong>Portabilité des données</strong></p>
                     <p>L'Éditeur s'engage à vous offrir la possibilité de vous faire restituer l'ensemble des données
                     vous concernant sur simple demande. L'Utilisateur se voit ainsi garantir une meilleure
                     maîtrise de ses données, et garde la possibilité de les réutiliser. Ces données devront être
